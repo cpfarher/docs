482b990651e013ccb6feff75e3a31dd052035645
Christian Pfarher <c.pfarher@gmail.com>
2025-04-16 12:48:27 -0300

    add other version

--- vanilla.js
+++ vanilla.js
@@ -16,8 +16,27 @@ function markDownToHtml(markdown) {
     const translateDict = { '#': 'h1', '##': 'h2', '###': 'h3' };
     const markDownTag = translateDict[markDownSymbol];
     console.log(`<${markDownTag}>${text}</${markDownTag}>`);
+}
+
+markDownToHtml('# hello #');
+markDownToHtml('## hello ads f ##');
+markDownToHtml('### hello ###');
+
+// better approach using regex => 
+function markDownToHtmlRegex(markdown) {
+  const match = markdown.match(/^(#+)\s+(.*?)\s+\1$/);
+
+  if (!match) {
+    console.log('Invalid markdown format');
+    return;
   }
 
-  markDownToHtml('# hello #');
-  markDownToHtml('## hello ads f ##');
-  markDownToHtml('### hello ###');
+  const [, hashes, text] = match;
+  const tag = `h${hashes.length}`;
+  console.log(`<${tag}>${text}</${tag}>`);
+}
+
+markDownToHtmlRegex('# hello #');
+markDownToHtmlRegex('## hello ads f ##');
+markDownToHtmlRegex('### hello ###');
+markDownToHtmlRegex('#### hello ####');
